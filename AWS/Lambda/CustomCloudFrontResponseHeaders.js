97f697a3ff080d57b2b1bcadf411dad4283120e2
Lucas Cantor <lucascantor@gmail.com>
2018-03-09 22:28:12 -0800

    reintroduce csp with in-line style and script hashes, connect-src

--- AWS/Lambda/CustomCloudFrontResponseHeaders.js
+++ AWS/Lambda/CustomCloudFrontResponseHeaders.js
@@ -24,6 +24,10 @@ exports.handler = (event, context, callback) => {
         key: 'Referrer-Policy',
         value: 'no-referrer',
     }];
+    response.headers['content-security-policy'] = [{
+        key: 'Content-Security-Policy',
+        value: "default-src 'none'; object-src 'none'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self'; script-src 'self' 'sha256-R67IfEAouXF37f93rvmLikOvP/7VsvCttb6uKYqzulI=' 'sha256-A1FYnT1QjW1PWpdCVyoIX2FsLqzmQevMHcpeuU5rU/w='; connect-src https://zs5c80k81f.execute-api.us-east-1.amazonaws.com/prod/ContactFormLambda; style-src 'self' https://fonts.googleapis.com 'sha256-nVXRYkfCoGFqhSXRlA/2bx5XqjOOAdp4F+lusvZgrno=' 'sha256-dsYDfz3Tdxrj6+jfSvUp+XQvhJPUWTFK4Jm9jbF+ll4=' 'sha256-tIs8OfjWm8MHgPJrHv7mM4wvA/FDFcra3Pd5icRMX+k=' 'sha256-xY3kjt1TMrF9YCbKikF0duY3dGEncCiKAQ3om+GN2N0='",
+    }];
 
     callback(null, response);
 };
