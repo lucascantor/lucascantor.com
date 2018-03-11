37ec95b0cebef9d27651f804b1ce75baaf2e9cdd
Lucas Cantor <lucascantor@gmail.com>
2018-03-10 19:39:58 -0800

    allow script-src from lucascantor.com

--- AWS/Lambda/CustomCloudFrontResponseHeaders.js
+++ AWS/Lambda/CustomCloudFrontResponseHeaders.js
@@ -26,7 +26,7 @@ exports.handler = (event, context, callback) => {
     }];
     response.headers['content-security-policy'] = [{
         key: 'Content-Security-Policy',
-        value: "default-src 'none'; object-src 'none'; base-uri 'none'; frame-ancestors 'none'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' https://i.imgur.com https://images.unsplash.com; script-src 'self'; connect-src https://zs5c80k81f.execute-api.us-east-1.amazonaws.com; style-src 'self' https://fonts.googleapis.com",
+        value: "default-src 'none'; object-src 'none'; base-uri 'none'; frame-ancestors 'none'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' https://i.imgur.com https://images.unsplash.com; script-src 'self' https://lucascantor.com; connect-src https://zs5c80k81f.execute-api.us-east-1.amazonaws.com; style-src 'self' https://fonts.googleapis.com",
     }];
 
     callback(null, response);
