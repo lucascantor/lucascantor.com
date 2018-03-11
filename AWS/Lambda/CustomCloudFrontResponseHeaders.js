b98cd5086ce5a15242e45f6f54024ed952363374
Lucas Cantor <lucascantor@gmail.com>
2018-03-10 19:34:33 -0800

    externalize inline style and scripts

--- AWS/Lambda/CustomCloudFrontResponseHeaders.js
+++ AWS/Lambda/CustomCloudFrontResponseHeaders.js
@@ -26,7 +26,7 @@ exports.handler = (event, context, callback) => {
     }];
     response.headers['content-security-policy'] = [{
         key: 'Content-Security-Policy',
-        value: "default-src 'none'; object-src 'none'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self'; script-src 'self' 'unsafe-inline' 'sha256-R67IfEAouXF37f93rvmLikOvP/7VsvCttb6uKYqzulI=' 'sha256-A1FYnT1QjW1PWpdCVyoIX2FsLqzmQevMHcpeuU5rU/w='; connect-src https://zs5c80k81f.execute-api.us-east-1.amazonaws.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+        value: "default-src 'none'; object-src 'none'; base-uri 'none'; frame-ancestors 'none'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' https://i.imgur.com https://images.unsplash.com; script-src 'self'; connect-src https://zs5c80k81f.execute-api.us-east-1.amazonaws.com; style-src 'self' https://fonts.googleapis.com",
     }];
 
     callback(null, response);
